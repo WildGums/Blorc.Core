96beec8d8c8fe7e36ef4a6a32c69d5654d97042c
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2019-12-26 13:59:36 -0500

    Load script in a Promise that returns after the script is loaded

--- src/Blorc.Core/wwwroot/injector.js
+++ src/Blorc.Core/wwwroot/injector.js
@@ -11,15 +11,16 @@ window.BlorcInjector = {
     },
 
     injectScript: function (source, type) {
-        var head = document.getElementsByTagName('head')[0];
+        return new Promise((resolve, reject) => {
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
+            script.onload = function () {
+                resolve(true);
+            };
 
-        var script = document.createElement('script');
-        script.onload = function ()
-        {
-        };
-
-        script.src = source;
-        script.type = type;
-        head.appendChild(script);
+            script.src = source;
+            script.type = type;
+            head.appendChild(script);
+        });
     }
 };
