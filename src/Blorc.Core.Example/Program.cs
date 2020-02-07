da2bfe21630a4f7bfa3b83285f019f0eb575a323
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-02-06 23:16:18 -0500

    Update to Blazor 3.2.0-preview1.20073.1

--- src/Blorc.Core.Example/Program.cs
+++ src/Blorc.Core.Example/Program.cs
@@ -1,18 +1,35 @@
 ﻿namespace Blorc.Example
 {
+    using System.Threading.Tasks;
+
+    using Blorc.Example.Shared;
+    using Blorc.Services;
+
     using Microsoft.AspNetCore.Blazor.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
 
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            // TODO: Register component services...
+            builder.Services.AddTransient<SurveyExecutionService>();
+            builder.Services.AddTransient<SurveyVisualizationService>();
+
+            builder.RootComponents.Add<App>("app");
+
+
+            var host = builder.Build();
+
+            var componentServiceFactory = host.Services.GetService<IComponentServiceFactory>();
+            componentServiceFactory.Map<SurveyPrompt, SurveyExecutionService>();
+            componentServiceFactory.Map<SurveyPrompt, SurveyVisualizationService>();
+
+
+            await host.RunAsync();
         }
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args)
-        {
-            return BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
-        }
     }
 }
