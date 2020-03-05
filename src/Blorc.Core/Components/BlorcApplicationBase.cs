ede780e8cb8126b05412d6cf248359398b28fbe6
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-03-04 23:29:45 -0500

    Add BlorcApplicationBase

---
+++ src/Blorc.Core/Components/BlorcApplicationBase.cs
@@ -0,0 +1,44 @@
+﻿namespace Blorc.Components
+{
+    using System.Threading.Tasks;
+
+    using Blorc.Services;
+
+    using Microsoft.AspNetCore.Components;
+
+    /// <summary>
+    /// The blorc application.
+    /// </summary>
+    public class BlorcApplicationBase : BlorcComponentBase
+    {
+        protected bool Initialized { get; private set; }
+
+        /// <summary>
+        ///     Gets or sets the document service.
+        /// </summary>
+        [Inject]
+        private IDocumentService DocumentService { get; set; }
+
+        /// <summary>
+        /// The on configure document.
+        /// </summary>
+        /// <param name="documentService">
+        /// The document service.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        protected virtual Task OnConfiguringDocument(IDocumentService documentService)
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await OnConfiguringDocument(DocumentService);
+            await base.OnInitializedAsync();
+            Initialized = true;
+            StateHasChanged();
+        }
+    }
+}
