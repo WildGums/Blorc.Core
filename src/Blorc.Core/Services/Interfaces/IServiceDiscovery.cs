e5903e4d5a1812658879ee89bc671ff879a63980
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-03-09 13:46:43 -0400

    Add IServiceDiscovery interface

---
+++ src/Blorc.Core/Services/Interfaces/IServiceDiscovery.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IServiceDiscovery.cs" company="WildGums">
+//   Copyright (c) 2008 - 2020 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Blorc.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The ServiceDiscovery interface.
+    /// </summary>
+    public interface IServiceDiscovery
+    {
+        /// <summary>
+        /// Gets service end point.
+        /// </summary>
+        /// <param name="serviceName">
+        /// The service name.
+        /// </param>
+        /// <param name="idx">
+        /// The idx.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        Task<string> GetServiceEndPoint(string serviceName, int idx = 0);
+    }
+}
