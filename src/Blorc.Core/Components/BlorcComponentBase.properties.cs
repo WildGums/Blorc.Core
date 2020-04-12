dde35f253bbb24995b7b964a359f91c8bc76f493
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-04-12 18:38:09 -0400

    Fix dictionary type to capture additional attributes

--- src/Blorc.Core/Components/BlorcComponentBase.properties.cs
+++ src/Blorc.Core/Components/BlorcComponentBase.properties.cs
@@ -20,7 +20,7 @@
         }
 
         [Parameter(CaptureUnmatchedValues = true)]
-        public IDictionary<string, object> AdditionalAttributes { get; set; }
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
         
         [Parameter]
         public bool DisableAutomaticRaiseEventCallback { get; set; }
