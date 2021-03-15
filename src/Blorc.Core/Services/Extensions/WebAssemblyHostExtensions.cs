d211a22a9553bf8fb46eabbd5cfd57c8976f4b81
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-03-14 22:19:39 -0400

    Fix extension methods to configure WebAssemblyHost

--- src/Blorc.Core/Services/Extensions/WebAssemblyHostExtensions.cs
+++ src/Blorc.Core/Services/Extensions/WebAssemblyHostExtensions.cs
@@ -1,23 +1,31 @@
 ﻿namespace Blorc.Services
 {
     using System;
-    using System.Data;
+    using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class WebAssemblyHostExtensions
     {
+        public static async Task ConfigureAsync(this WebAssemblyHost @this, Func<IServiceProvider, Task> options)
+        {
+            var requiredService = @this.Services.GetRequiredService<IServiceProvider>();
+            await options(requiredService);
+            await @this.RunAsync();
+        }
+
+        public static async Task ConfigureDocumentAsync(this WebAssemblyHost @this, Func<IDocumentService, Task> options)
+        {
+            var requiredService = @this.Services.GetRequiredService<IDocumentService>();
+            await options(requiredService);
+            await @this.RunAsync();
+        }
+
         public static WebAssemblyHost MapComponentServices(this WebAssemblyHost @this, Action<IComponentServiceFactory> options)
         {
             options?.Invoke(@this.Services.GetRequiredService<IComponentServiceFactory>());
             return @this;
-        } 
-
-        public static WebAssemblyHost ConfigureDocument(this WebAssemblyHost @this, Action<IDocumentService> options)
-        {
-            options?.Invoke(@this.Services.GetRequiredService<IDocumentService>());
-            return @this;
-        } 
+        }
     }
 }
